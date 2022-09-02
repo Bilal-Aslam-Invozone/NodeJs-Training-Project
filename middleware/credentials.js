71b97271a54c1cc4bec3bba15024d19a1d0d20d0
Bilal-Aslam-Invozone <bilal.aslam@invozone.com>
2022-09-02 12:39:51 +0500

    finalised project

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -21,37 +21,3 @@ module.exports = async (req, res, next) =>{
         })
     };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const jwt = require ("jsonwebtoken") ;
-
-// const auth = ( req , res , next ) => {
-//    try {                    
-//     console.log("HTTP Method:  "+req.method+" , URL:  ",req.url)
-//        let token =req.headers.authorization ;
-//        if ( token ){
-//             token = token.split (" ")[1] ;
-//             let user=jwt.verify (token , "skdfii9sdijjisdmjj9eds") ;
-//             req.userId = user.id ;
-//        }
-//        else 
-//        {
-//             res.status(401).json( {message : " Unauthorized User " } ) ;
-//        }
-//        next ( ) ;
-//     } catch ( error ) {
-//         console.log ( error ) ;
-//        res.status(401).json({message : " Unauthorized User "}) ;    
-//     }}
-// module.exports= auth ;
